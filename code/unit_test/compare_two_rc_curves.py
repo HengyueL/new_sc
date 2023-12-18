e62bd26936da51212c2d3e8d4aca75e3a74b3f63
liang hengyue <lianghengyue1993@outlook.com>
2023-12-18 12:35:01 -0600

    Add

--- code/unit_test/compare_two_rc_curves.py
+++ code/unit_test/compare_two_rc_curves.py
@@ -41,7 +41,7 @@ def main():
             else:
                 for name in acc_plot_x_names:
                     acc_values.append(acc_dict[name])
-        if "p2" in case.lower():
+        if "marginstd" in case.lower():
             ls, marker = "dotted", None
         elif "ce" in case.lower():
             ls, marker = "solid", None
