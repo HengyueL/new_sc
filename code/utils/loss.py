65bdbdae1636f8ae00eba30a8a6cd3e8b0435b70
liang hengyue <lianghengyue1993@outlook.com>
2023-12-19 16:35:13 -0600

    Add Margin Loss 2nd logit stop grad

--- code/utils/loss.py
+++ code/utils/loss.py
@@ -4,6 +4,52 @@ import torch.nn.functional as F
 
 
 # ==== Regular Loss (for minimization, or grandient ascent) ====
+# class MarginLoss(nn.Module):
+#     """
+#         AKA CW loss:
+#             for a 1D vector x and a label class y
+#             loss = max(0, margin - x[y] + x[i])**p, where x[i] = max x and i != y.
+#     """
+#     def __init__(self, reduction="mean", margin=1, p=1, rescale_logits=False, temperature=1):
+#         super(MarginLoss, self).__init__()
+#         assert reduction in ["mean", "sum", "none", None], "Reduction needs to be within ['mean', 'sum', 'none', None]"
+#         self.reduction = reduction
+#         self.margin = margin
+#         self.p = p
+#         self.rescale_logits = rescale_logits
+#         self.t = temperature
+
+#     def forward(self, logits, labels):
+        
+#         if self.rescale_logits:
+#             # === This one implements the logit normalization ===
+#             logit_norms = torch.norm(logits, p=2, dim=-1, keepdim=True) + 1e-7
+#             logits = torch.div(logits, logit_norms) / self.t
+
+#         correct_logits = torch.gather(logits, 1, labels.view(-1, 1)) # [n, 1]  --- x[y]
+#         max_2_logits, argmax_2_logits = torch.topk(logits, 2, dim=1)
+#         top_max, second_max = max_2_logits.chunk(2, dim=1)
+#         top_argmax, _ = argmax_2_logits.chunk(2, dim=1)
+
+#         labels_eq_max = top_argmax.squeeze().eq(labels).float().view(-1, 1)
+#         labels_ne_max = top_argmax.squeeze().ne(labels).float().view(-1, 1)
+#         max_incorrect_logits = labels_eq_max * second_max + labels_ne_max * top_max # [n, 1] --  x[i]
+
+#         loss = torch.clamp(self.margin - correct_logits + max_incorrect_logits, min=0)
+
+#         # If use power values
+#         if self.p != 1:
+#             loss = loss ** self.p
+
+#         # If valid reduction 
+#         if self.reduction == "mean":
+#             loss = loss.mean()
+#         elif self.reduction == "sum":
+#             loss = loss.sum()
+
+#         return loss
+
+
 class MarginLoss(nn.Module):
     """
         AKA CW loss:
@@ -35,7 +81,7 @@ class MarginLoss(nn.Module):
         labels_ne_max = top_argmax.squeeze().ne(labels).float().view(-1, 1)
         max_incorrect_logits = labels_eq_max * second_max + labels_ne_max * top_max # [n, 1] --  x[i]
 
-        loss = torch.clamp(self.margin - correct_logits + max_incorrect_logits, min=0)
+        loss = torch.clamp(self.margin - correct_logits + max_incorrect_logits.detach(), min=0)
 
         # If use power values
         if self.p != 1:
@@ -64,19 +110,15 @@ class LogitNormLoss(nn.Module):
 
 
 if __name__ == "__main__":
-    for _ in range(10):
-        x = torch.tensor([[0.1, 0.2, 0.4, 0.8]])
-        y = torch.tensor([3])
+    x = torch.tensor([[0.1, 0.2, 0.4, 0.8]])
+    y = torch.tensor([2])
 
-        test_loss = MarginLoss("mean", rescale_logits=True)
+    test_loss = MarginLoss("mean", rescale_logits=False)
 
-        loss_1 = test_loss(x, y)
-        print("===========")
-        print(x)
-        print(y)
-        print(loss_1)
-        print("Shape:", y.shape)
-        print("Shape:", loss_1.shape)
-        # print(loss_dlr.item())
-
-    print()
+    loss_1 = test_loss(x, y)
+    print("===========")
+    print(x)
+    print(y)
+    print(loss_1)
+    print("Shape:", y.shape)
+    print("Shape:", loss_1.shape)
